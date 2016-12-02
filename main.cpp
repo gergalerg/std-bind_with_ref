05ab7290a17a69319eb2929a18159787ce7b856f
Greg Russell <gregspace@gmail.com>
2016-12-01 16:45:20 -0800

    Before 27.4.1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <string>
+#include <typeinfo>
+#include <map>
 
-enum State {empty, nought, cross };
 
 int main(int argc, char const *argv[])
 {
-    std::vector<State> pos;
-    pos.push_back(cross);
-    pos.push_back(nought);
-    pos.push_back(empty);
+    std::map<char, int> mymap{};
 
-    for (auto x: pos) std::cout << (int)x << "\n";
-    return 0;
+
 }
+
